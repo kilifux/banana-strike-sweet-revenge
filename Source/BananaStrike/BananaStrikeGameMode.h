c05ed248429ac1a7068f86f01cca037a70557c6f
Mateusz Banasik <kilifugame@gmail.com>
2023-04-15 21:08:50 +0200

    Initial project commit - Unreal Third Person Template

---
+++ Source/BananaStrike/BananaStrikeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BananaStrikeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABananaStrikeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABananaStrikeGameMode();
+};
+
+
+
